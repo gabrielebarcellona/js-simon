fdb5e4287c7be6f6c2bff5b66257ba2c5cc57c68
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-04-27 17:46:51 +0200

    add edit css

--- script.js
+++ script.js
@@ -20,24 +20,24 @@ let x = setInterval(function() {
   let now = new Date().getTime();
 
   
-  let distance = countDownDate - now;
+  let difference = countDownDate - now;
 
  
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
   
   document.getElementById("output").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   
-  if (distance < 0) {
+  if (difference < 0) {
     clearInterval(x);
     document.getElementById("output").innerHTML = "EXPIRED";
   }
-}, 1000);
+}, );
 
 
 
